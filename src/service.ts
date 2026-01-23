8feadf975db3e3946e0339db817e581b7f1ffb38
Forgata <gombwadivine@gmail.com>
2026-01-23 21:49:37 +0200

    test system proxy reader

--- src/service.ts
+++ src/service.ts
@@ -1,2 +1,9 @@
-import { startTcpServer } from "./core/services/tcp/tcpServer.js";
-startTcpServer();
+// import { startTcpServer } from "./core/services/tcp/tcpServer.js";
+// startTcpServer();
+
+import { readSystemProxy } from "./core/services/proxy/proxyReader.js";
+
+async function main() {
+  const proxy = await readSystemProxy();
+  console.log("[Deadbolt][DEBUG] System Proxy State:", proxy);
+}
