cce08f339b2a719ff159d05480afaf0058f583e5
Forgata <gombwadivine@gmail.com>
2026-01-25 00:04:57 +0200

    refactor TCP listener starter

--- src/core/services/tcp/tcpServer.ts
+++ src/core/services/tcp/tcpServer.ts
@@ -4,16 +4,24 @@ import { handleConnection } from "./connectionHandler.js";
 const HOST = "127.0.0.1";
 const PORT = 7890;
 
-export function startTcpServer() {
-  const server = net.createServer((socket) => {
-    handleConnection(socket);
-  });
-  server.on("error", (error) => {
-    console.error("[TCP SERVER ERROR]", error);
-  });
+export function startTcpServer(
+  handleConnection: (socket: net.Socket) => void,
+  PORT: number,
+  HOST: string,
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer((socket) => {
+      handleConnection(socket);
+    });
+    server.once("error", (error) => {
+      console.error("[TCP SERVER ERROR]", error);
+      reject(error);
+    });
 
-  //   listening
-  server.listen(PORT, HOST, () => {
-    console.log(`[Deadbolt] TCP Listener running on ${HOST}:${PORT}`);
+    //   listening
+    server.listen(PORT, HOST, () => {
+      console.log(`[Deadbolt] TCP Listener running on ${HOST}:${PORT}`);
+      resolve();
+    });
   });
 }
