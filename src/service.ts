0c3a3aec8692e3c612fa81fcf9156e71ba5dcecb
Forgata <gombwadivine@gmail.com>
2026-01-25 00:06:38 +0200

    refactor system entry point with save mode

--- src/service.ts
+++ src/service.ts
@@ -1,23 +1,42 @@
 import { startTcpServer } from "./core/services/tcp/tcpServer.js";
-// startTcpServer();
-
 import { determineProxyState } from "./core/services/proxy/proxyController.js";
 import { readProxyConfig } from "./core/services/proxy/proxyReader.js";
-import { testCases } from "./core/services/proxy/test/index.js";
+import { ensureBackup } from "./core/services/proxy/proxyBackup.js";
+import { handleConnection } from "./core/services/tcp/connectionHandler.js";
+import { restoreProxyFromBackup } from "./core/services/proxy/restoreBackup.js";
+import { setDeadboltProxy } from "./core/services/proxy/setDeadboltProxy.js";
+
+const HOST = "127.0.0.1";
+const PORT = 7890;
+const DEADBOLT_SAFE = 1;
 
 async function main() {
-  const proxy = await readProxyConfig();
-  console.log("[Deadbolt][DEBUG] System Proxy State:", proxy);
+  const config = await readProxyConfig();
+  const state = determineProxyState(config, HOST, PORT, false);
 
-  for (const t of testCases) {
-    const state = determineProxyState(
-      t.config,
-      "127.0.0.1",
-      7890,
-      t.listenerRunning,
-    );
-    console.log(state);
+  try {
+    // backup before anything
+    await ensureBackup(state, config);
+
+    // start tcp server
+    await startTcpServer(handleConnection, PORT, HOST);
+
+    // system proxy mutation if not safe mode (testing)
+    if (!DEADBOLT_SAFE) {
+      await setDeadboltProxy(HOST, PORT);
+      console.log("[DEADBOLT] Proxy safely bound to listener");
+    } else {
+      console.warn("[DEADBOLT] SAFE MODE — proxy mutation skipped");
+    }
+  } catch (error) {
+    console.error("[DEADBOLT] Startup error", error);
+    await restoreProxyFromBackup();
+    process.exit(1);
   }
+
+  process.on("SIGINT", async () => await restoreProxyFromBackup);
+  process.on("SIGTERM", async () => await restoreProxyFromBackup);
+  process.on("exit", async () => await restoreProxyFromBackup);
 }
 
 main().catch((error) => console.error(error));
