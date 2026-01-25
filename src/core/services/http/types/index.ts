0928973d7ff9aa0729787b19817881fdf7498260
Forgata <gombwadivine@gmail.com>
2026-01-26 00:56:16 +0200

    add httpHeaderParser types and return interface

--- src/core/services/http/types/index.ts
+++ src/core/services/http/types/index.ts
@@ -8,3 +8,12 @@ export interface DecodedHeaders {
   raw: string;
   lines: string[];
 }
+
+export type HttpVersion = "HTTP/1.0" | "HTTP/1.1";
+
+export interface ParsedHttpRequest {
+  method: string;
+  target: string;
+  version: HttpVersion;
+  headers: Map<string, string>;
+}
