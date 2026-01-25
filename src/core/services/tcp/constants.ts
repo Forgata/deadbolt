920ab6fad223c7adc4d22192a7a4f84adfff5e45
Forgata <gombwadivine@gmail.com>
2026-01-25 13:35:05 +0200

    create a constants file for storage

--- src/core/services/tcp/constants.ts
+++ src/core/services/tcp/constants.ts
@@ -1,4 +1,4 @@
-const SOCKET_TIMEOUT_MS = 5000;
-const MAX_PREVIEW_BYTES = 1024;
+export const SOCKET_TIMEOUT_MS = 5000;
+export const MAX_PREVIEW_BYTES = 1024;
 export const MAX_HEADER_BYTES = 16 * 1024;
 export const HEADER_TERMINATOR = Buffer.from("\r\n\r\n");
