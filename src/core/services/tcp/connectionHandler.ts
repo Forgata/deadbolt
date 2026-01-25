bbf8b16b29c5809ac9da0dd2b70e57f1cacd146a
Forgata <gombwadivine@gmail.com>
2026-01-25 12:54:28 +0200

    implement connectionContext in TCP handler function

--- src/core/services/tcp/connectionHandler.ts
+++ src/core/services/tcp/connectionHandler.ts
@@ -1,37 +1,40 @@
 import net from "node:net";
 import crypto from "node:crypto";
+import { createConnectionContext } from "./connectionContext.js";
 
 const SOCKET_TIMEOUT_MS = 5000;
 const MAX_PREVIEW_BYTES = 1024;
 
 export function handleConnection(socket: net.Socket) {
-  const connectionId = crypto.randomUUID();
+  const ctx = createConnectionContext();
   console.log(
-    `[${connectionId}] Connection opened from ${socket.remoteAddress}:${socket.remotePort}`,
+    `[${ctx.id}] Connection opened from ${socket.remoteAddress}:${socket.remotePort}`,
   );
 
   socket.setTimeout(SOCKET_TIMEOUT_MS);
 
   socket.on("data", (chunk: Buffer) => {
+    ctx.bytesReceived += chunk.length;
+    ctx.buffer = Buffer.concat([ctx.buffer, chunk]);
     const preview = chunk.subarray(0, MAX_PREVIEW_BYTES);
-    console.log(`[${connectionId}] Received ${chunk.length}`);
-    console.log(`[${connectionId}] Preview  \n${preview.toString("utf8")}`);
+    console.log(`[${ctx.id}] Received ${chunk.length}`);
+    console.log(`[${ctx.id}] Preview  \n${preview.toString("utf8")}`);
     socket.end();
   });
 
   //   on timeot
   socket.on("timeout", () => {
-    console.warn(`[${connectionId}] socket timed out`);
+    console.warn(`[${ctx.id}] socket timed out`);
     socket.destroy();
   });
 
   //   on error
   socket.on("error", (error) => {
-    console.error(`[${connectionId}] socket error`, error);
+    console.error(`[${ctx.id}] socket error`, error);
     socket.destroy();
   });
 
   socket.on("close", () => {
-    console.log(`[${connectionId}] Connection closed`);
+    console.log(`[${ctx.id}] Connection closed`);
   });
 }
