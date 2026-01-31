11503e81ff94c427bd2bea5b662f7e33810066aa
Forgata <gombwadivine@gmail.com>
2026-01-31 15:55:40 +0200

    update constants for parseTlsHeaderRecord

--- src/core/services/tls/constants.ts
+++ src/core/services/tls/constants.ts
@@ -1,2 +1,6 @@
 export const TLS_HEADER_SIZE = 5;
 export const MAX_TLS_RECORD_SIZE = 16 * 1024 + TLS_HEADER_SIZE;
+
+// tls record parse constants
+export const TLS_RECORD_HEADER_SIZE = 5;
+export const MAX_TLS_RECORD_LENGTH = 16 * 1024;
