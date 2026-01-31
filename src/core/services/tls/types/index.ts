75f7c7e4b2ae3f56bb07f4263e23ae3188c05cc7
Forgata <gombwadivine@gmail.com>
2026-01-31 15:56:38 +0200

    add interface for return types for the parseTlsHeaderRecord ts file

--- src/core/services/tls/types/index.ts
+++ src/core/services/tls/types/index.ts
@@ -8,3 +8,22 @@ export interface TlsBufferState {
   buffer: Buffer;
   expectedLength: number | null;
 }
+
+// tls record parse results
+export type TlsRecordParseResult =
+  | {
+      status: "INCOMPLETE";
+      neededBytes: number;
+    }
+  | {
+      status: "INVALID";
+      reason: string;
+    }
+  | {
+      status: "COMPLETE";
+      contentType: number;
+      versionMajor: number;
+      versionMinor: number;
+      recordLength: number;
+      recordEndOffset: number;
+    };
